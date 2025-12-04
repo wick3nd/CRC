20654327a250eb01ac74802ea5d60637353fff0c
wickend <80926418+wick3nd@users.noreply.github.com>
2025-12-04 20:37:39 +0100

    CRC32Stream finished + misc
    CRC32Stream validation and calculation is finished and ready for use
    Readme.md update is near

--- src/w3.CRC/CRC32Stream.cs
+++ src/w3.CRC/CRC32Stream.cs
@@ -1,79 +1,160 @@
 ﻿using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.Arm;
+using System.Runtime.Intrinsics.X86;
 
 namespace w3.CRC
 {
-    public class CRC32Stream : Stream
+    public class CRC32Stream
     {
         private readonly Stream _inner;
 
         private byte[]? _buffer;
 
-        public override long Length => _inner.Length;
-        public override long Position { get => throw new NotSupportedException(); set => throw new NotSupportedException(); }
-        
-        public override bool CanRead => false;
-        public override bool CanWrite => false;
-        public override bool CanSeek => false;
-
         public CRC32Stream(Stream inner)
         {
-            if (!inner.CanSeek) throw new NotSupportedException("The stream is not seekable. ");
+            if (!inner.CanSeek) throw new NotSupportedException("The stream is not seekable.");
             _inner = inner;
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public uint ComputeChecksum(int blockSize, int offset, int count)
         {
-            ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(offset, count);
             ArgumentOutOfRangeException.ThrowIfNegativeOrZero(blockSize);
             ArgumentOutOfRangeException.ThrowIfNegativeOrZero(count);
             ArgumentOutOfRangeException.ThrowIfNegative(offset);
 
-            uint crc = uint.MaxValue;
+            return ComputeChecksumInternal(blockSize, offset, count);
+        }
 
+        private uint ComputeChecksumInternal(int blockSize, int offset, int count)
+        {
             _buffer = new byte[blockSize];
             _inner.Seek(offset, SeekOrigin.Begin);
 
-            while (count > 0)
+            if (Sse42.IsSupported) return ComputeIntelCRCStream(count);
+            else return ComputeAmdCRCStream(count);
+        }
+
+        private uint ComputeIntelCRCStream(int count)
+        {
+            uint crc = 0xFFFFFFFF;
+            int remaining = count;
+
+            while (remaining > 0)
             {
-                int toRead = Math.Min(blockSize, count);
+                int toRead = Math.Min(_buffer!.Length, remaining);
                 int read = _inner.Read(_buffer, 0, toRead);
 
                 if (read <= 0) break;
+
                 int i = 0;
-
-                ref byte start = ref MemoryMarshal.GetReference(_buffer.AsSpan(0, read));
-
                 while (i + 4 <= read)
                 {
-                    crc = PrecomputedTables.CatagnolliTable[(byte)(crc ^ Unsafe.Add(ref start, i))] ^(crc >> 8);
-                    crc = PrecomputedTables.CatagnolliTable[(byte)(crc ^ Unsafe.Add(ref start, i + 1))] ^ (crc >> 8);
-                    crc = PrecomputedTables.CatagnolliTable[(byte)(crc ^ Unsafe.Add(ref start, i + 2))] ^ (crc >> 8);
-                    crc = PrecomputedTables.CatagnolliTable[(byte)(crc ^ Unsafe.Add(ref start, i + 3))] ^ (crc >> 8);
-
+                    crc = Sse42.Crc32(crc, BitConverter.ToUInt32(_buffer, i));
                     i += 4;
                 }
 
-                for (; i < read; i++) crc = (crc >> 8) ^ PrecomputedTables.CatagnolliTable[(byte)(crc ^ Unsafe.Add(ref start, i))];
+                while (i < read)
+                {
+                    crc = Sse42.Crc32(crc, _buffer[i]);
+                    i++;
+                }
 
-                count -= read;
-                offset += read;
+                remaining -= read;
             }
 
             return ~crc;
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool Validate()
+        private uint ComputeAmdCRCStream(int count)
         {
-            return false;
+            uint crc = 0xFFFFFFFF;
+            int remaining = count;
+
+            while (remaining > 0)
+            {
+                int toRead = Math.Min(_buffer!.Length, remaining);
+                int read = _inner.Read(_buffer, 0, toRead);
+
+                if (read <= 0) break;
+
+                int i = 0;
+                while (i + 4 <= read)
+                {
+                    crc = Crc32.ComputeCrc32C(crc, BitConverter.ToUInt32(_buffer, i));
+                    i += 4;
+                }
+
+                while (i < read)
+                {
+                    crc = Crc32.ComputeCrc32C(crc, _buffer[i]);
+                    i++;
+                }
+
+                remaining -= read;
+            }
+
+            return ~crc;
         }
 
-        public override int Read(byte[] buffer, int offset, int count) => throw new NotSupportedException();
-        public override void Write(byte[] buffer, int offset, int count) => throw new NotSupportedException();
-        public override long Seek(long offset, SeekOrigin origin) => throw new NotSupportedException();
-        public override void SetLength(long value) => throw new NotSupportedException();
-        public override void Flush() => _inner.Flush();
+       // Inferior
+       /*
+        private uint ComputeCustomCRCStream(int count)
+        {
+            uint crc = 0xFFFFFFFF;
+            int remaining = count;
+
+            while (remaining > 0)
+            {
+                int toRead = Math.Min(_buffer!.Length, remaining);
+                int read = _inner.Read(_buffer, 0, toRead);
+
+                if (read <= 0) break;
+
+                int i = 0;
+                ref byte start = ref MemoryMarshal.GetReference(_buffer.AsSpan(0, read));
+
+                while (i + 4 <= read)
+                {
+                    crc = PrecomputedTables.CatagnolliTable[(byte)(crc ^ Unsafe.Add(ref start, i))] ^ (crc >> 8);
+                    crc = PrecomputedTables.CatagnolliTable[(byte)(crc ^ Unsafe.Add(ref start, i + 1))] ^ (crc >> 8);
+                    crc = PrecomputedTables.CatagnolliTable[(byte)(crc ^ Unsafe.Add(ref start, i + 2))] ^ (crc >> 8);
+                    crc = PrecomputedTables.CatagnolliTable[(byte)(crc ^ Unsafe.Add(ref start, i + 3))] ^ (crc >> 8);
+                    i += 4;
+                }
+
+                for (; i < read; i++) crc = (crc >> 8) ^ PrecomputedTables.CatagnolliTable[(byte)(crc ^ Unsafe.Add(ref start, i))];
+
+                remaining -= read;
+            }
+
+            return ~crc;
+        }
+       */
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Validate(int blockSize, int offset, int count)
+        {
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(blockSize);
+            ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(count-4, 0);
+            ArgumentOutOfRangeException.ThrowIfNegative(count);
+            ArgumentOutOfRangeException.ThrowIfNegative(offset);
+
+            return ValidateInternal(blockSize, offset, count);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private bool ValidateInternal(int blockSize, int offset, int count)
+        {
+            uint recalcCRC;
+            uint fileCRC;
+            byte[] temp = new byte[4];
+
+            recalcCRC = ComputeChecksum(blockSize, offset, count-4);
+
+            _inner.Read(temp, 0, 4);
+            fileCRC = BitConverter.ToUInt32(temp);
+
+            return recalcCRC == fileCRC;
+        }
     }
 }
